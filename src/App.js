0986efbbb744f15d56652873a525277b225d6a43
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-02-04 22:08:11 +0300

    design changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Landing from "./components/Landing";
 import Products from './components/Products';
 import About from './components/About';
@@ -7,10 +7,23 @@ import AboutCompany from './components/AboutCompany';
 import Map from './components/Map';
 import Materials from './components/Materials';
 import Footer from './components/Footer';
+import Loading from './components/Loading';
 
 function App() {
+  // const [ isLoading, setIsLoading ] = useState(false);
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 2000)
+  // }, [])
+
+  // if(isLoading){
+  //   <Loading  />
+  // }else 
   return (
     <div className="App">
+      {/* <Loading  /> */}
       <Landing />
       <Products />
       <About />
---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='loadingContainer'>
+        <h1>MegaKorm</h1>
+    </div>
+  )
+}
+
+export default Loading
